d40741f1ada60bef98a78cde60dcbb69af0313d7
dongt <dongt@LAPTOP-VGVMTP5E>
2022-09-03 16:49:34 +0700

    Add project files.

---
+++ Calculator/Program.cs
@@ -0,0 +1,19 @@
+﻿using CalculatorLibrary;
+
+// Display title as the C# console calculator app.
+Console.WriteLine("Console Calculator in C#\r");
+Console.WriteLine("------------------------\n");
+
+// Ask the user to type the first number.
+Console.WriteLine("Type a number, and then press Enter");
+var num1 = Convert.ToInt32(Console.ReadLine());
+
+// Ask the user to type the second number.
+Console.WriteLine("Type another number, and then press Enter");
+var num2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"Your result: {num1} + {num2} = " + (Calculator.DoSum(num1, num2)));
+
+// Wait for the user to respond before closing.
+Console.Write("Press any key to close the Calculator console app...");
+Console.ReadKey();
---
+++ CalculatorLibrary/Calculator.cs
@@ -0,0 +1,10 @@
+﻿namespace CalculatorLibrary
+{
+    public class Calculator
+    {
+        public static int DoSum(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+    }
+}
